20e74c002fbf3cb40f6d2966baec4d7e2be62eec
SmaylinRobles <80171420+Smaylin@users.noreply.github.com>
2022-07-27 11:33:46 -0400

    Update Sql.sql

--- Sql.sql
+++ Sql.sql
@@ -1,3 +1,3 @@
 SELECT * FROM db_biblioteca;
 use db_biblioteca_backend;
-Insert into db_biblioteca values(1,"pablo","adfadfasf","sansa",'2018-01-03',"asdfasdf","terror",true,"la Polla Gorda")
+Insert into db_biblioteca values(3,"pablo","descripcion","sansa",'2018-01-03',"foto","terror",true,"El principito", 200)
